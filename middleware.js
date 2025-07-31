3fa4862da63825637c14174f6afb2e3537297033
AndyVanLandhof <land.andrew@gmail.com>
2025-07-31 22:06:33 +0100

    Add password protection middleware

---
+++ middleware.js
@@ -0,0 +1,27 @@
+export function middleware(req) {
+  const basicAuth = req.headers.get('authorization')
+
+  const USER = 'admin'
+  const PASS = 'mypassword'
+
+  if (basicAuth) {
+    const [scheme, encoded] = basicAuth.split(' ')
+    const decoded = atob(encoded)
+    const [user, pass] = decoded.split(':')
+
+    if (user === USER && pass === PASS) {
+      return new Response(null, { status: 200 })
+    }
+  }
+
+  return new Response('Authentication required', {
+    status: 401,
+    headers: {
+      'WWW-Authenticate': 'Basic realm="Secure Area"',
+    },
+  })
+}
+
+export const config = {
+  matcher: '/((?!api|_next/static|_next/image|favicon.ico).*)',
+} 
